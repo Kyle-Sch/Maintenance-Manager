19d3760a0c44f44b5ee342e8a82265b1cb53fa93
Kyle Schneiderlochner <kyle.david@comcast.net>
2019-08-03 11:27:38 -0400

    first commit

---
+++ Maintenance-Manager/src/main/java/com/maintain/MaintenanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.maintain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceManagerApplication.class, args);
+	}
+
+}
