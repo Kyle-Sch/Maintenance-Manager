5857bcc13a74f50eb2521f024af3a8ebf72e7a53
Kyle Schneiderlochner <kyle.david@comcast.net>
2019-08-04 16:35:28 -0400

    updating all branches

--- maintenance-service/src/test/java/com/maintain/MaintenanceServiceApplicationTests.java
+++ maintenance-service/src/test/java/com/maintain/MaintenanceServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MaintenanceManagerApplicationTests {
+public class MaintenanceServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
